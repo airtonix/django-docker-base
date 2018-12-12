25bb84f9e7aa676b08561ec08a01bb06fb5aad15
Zenobius Jiricek <airtonix@gmail.com>
2018-12-13 08:31:20 +1030

    Fix: fine tune main app. add api url and homepage

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""App URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
+
+from .views import HomePageView
 
 urlpatterns = [
+    path('polls/', include('polls.urls'), name='polls'),
+    path('jet/', include('jet.urls', 'jet'), name='admin'),  # Django JET URLS
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'), name='api'),
+    path('', HomePageView.as_view(), name='hpme'),
 ]
